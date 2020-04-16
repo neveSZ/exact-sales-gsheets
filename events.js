9061ac7ddb53f4a9a9bc7e76d911b73731c5be4d
Victor Neves Silva <38274108+neveSZ@users.noreply.github.com>
2020-04-16 00:18:00 -0300

    create events

---
+++ events.js
@@ -0,0 +1,20 @@
+function leadInserted(data){
+    console.log(data);
+}
+
+function schedule(data){
+    console.log(data);
+}
+
+function leadQualified(data){
+    console.log(data);
+}
+
+function leadLost(data){
+    console.log(data);
+}
+
+exports.leadInserted = leadInserted;
+exports.schedule = schedule;
+exports.leadQualified = leadQualified;
+exports.leadLost = leadLost;
