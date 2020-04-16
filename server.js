30c7eb0b147813c945a7ce66ef82393433c94d9f
Victor Neves Silva <38274108+neveSZ@users.noreply.github.com>
2020-04-16 16:29:49 -0300

    Console log custom field

--- server.js
+++ server.js
@@ -10,26 +10,26 @@ app.use(bodyParser.json());
 app.post("/leadinserted", (req, res) => {
     res.sendStatus(200);
     const data = req.body;
-    console.log(data);
+    console.log(data.Lead.CamposPersonalizados);
     events.leadInserted(data);
 });
 
 app.post("/schedule", (req, res) => {
     res.sendStatus(200);
     const data = req.body;
-    events.schedule(data);
+    console.log(data.Lead.CamposPersonalizados);
 });
 
 app.post("/leadqualified", (req, res) => {
     res.sendStatus(200);
     const data = req.body;
-   events.leadQualified(data);
+    console.log(data.Lead.CamposPersonalizados);
 });
 
 app.post("/leadlost", (req, res) => {
     res.sendStatus(200);
     const data = req.body;
-    events.leadLost(data);
+    console.log(data.Lead.CamposPersonalizados);
 });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
